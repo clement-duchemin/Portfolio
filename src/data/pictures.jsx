0ad59f1337be0b5fc38d6eab38f0dc724ea7511d
Clement Duchemin <clementduchemin19@gmail.com>
2023-06-03 17:07:27 +0200

    pics-opt

--- src/data/pictures.jsx
+++ src/data/pictures.jsx
@@ -1,9 +1,9 @@
 const pictures = [
     
-    '/images/sky-view.jpg',
+    '/images/sky-view-opt.jpg',
     '/images/old-wall-opt.jpg',
     '/images/neon-corridor-opt.jpg',
-    '/images/wall.jpg'
+    '/images/wall-opt.jpg'
     
 ]
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Title from "../../components/Title/Title"
 import HardSkills from "../../components/HardSkills/HardSkills"
 import Projects from "../../components/Projects/Projects"
 import Collapse from "../../components/Collapse/Collapse"
-import bkgrImage from "../../assets/sky-view.jpg"
+import bkgrImage from "../../assets/sky-view-opt.jpg"
 //import bkgrImage from "../../assets/red-wall.jpg"
 
 function Home() {
